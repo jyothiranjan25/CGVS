173046899c011be92cdadf57856253dd362f714a
kumar jyothiranjan <jyothiranjan25@gmail.com>
2025-04-12 20:59:40 +0530

    Update certificateGeneration.php

--- public/code/certificateGeneration.php
+++ public/code/certificateGeneration.php
@@ -111,7 +111,7 @@ function generateCertificate($regNo, $name, $startDate, $endDate, $course, $QrCo
     unlink($qrCodeImage);
 
 
-    // return $savePath;
+    return $savePath;
 
     // Test the certificate
     $certificateUrl = $savePath['url'];
